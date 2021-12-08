2878a70e0a0ce9a4884f6a51965b863d82608661
Semih Celek <cybermoon1999@gmail.com>
2021-12-08 21:57:12 +0300

    Update Platform Manager to spawn longer tiles

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlatformManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] platformPrefabs;
-    [SerializeField] private float tileLenght=15;
+    [SerializeField] private float tileLenght=60;
     private List<GameObject> activeTiles = new List<GameObject>();
 
     private Transform PlayerPosition;
@@ -23,10 +23,10 @@ public class PlatformManager : MonoBehaviour
     }
     private void Update()
     {
-        if (PlayerPosition.position.z -30 > zOffset-((platformPrefabs.Length)*tileLenght))
+        if (PlayerPosition.position.z -120 > zOffset-((platformPrefabs.Length)*tileLenght))
         {
             SpawnTile(Random.Range(0, platformPrefabs.Length));
-            Debug.Log(zOffset);
+            //Debug.Log(zOffset);
             DeleteTile();
         }
 
@@ -36,7 +36,7 @@ public class PlatformManager : MonoBehaviour
 
     private void SpawnTile(int tileIndex)
     {
-       GameObject tile = Instantiate(platformPrefabs[tileIndex], new Vector3(0,-8, 1 * zOffset) , Quaternion.Euler(0,90,0));
+       GameObject tile = Instantiate(platformPrefabs[tileIndex], new Vector3(0,-8, 1 * zOffset) , Quaternion.Euler(-90,90,0));
         zOffset += tileLenght;
         activeTiles.Add(tile);
 
