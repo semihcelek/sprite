19e252d7f1a05e81af5d00bca2431d8c473bb3a4
Semih Celek <cybermoon1999@gmail.com>
2021-12-18 00:41:39 +0300

    Modify varible name in CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Transform target;
-    private Vector3 offset;
+    private Transform _target;
+    private Vector3 _offset;
     [SerializeField] private float smoothTime = 0.5f;
 
-    private Vector3 cameraVelocity = Vector3.zero;
+    private Vector3 _cameraVelocity = Vector3.zero;
 
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-        offset = transform.position - target.position;
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+        _offset = transform.position - _target.position;
 
     }
+    
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 newPosition = new Vector3(offset.x + target.position.x, offset.y + target.position.y, offset.z+target.position.z);
+        Vector3 newPosition = new Vector3(_offset.x + _target.position.x, _offset.y + _target.position.y, _offset.z+_target.position.z);
         //transform.position = Vector3.Lerp(transform.position, newPosition,10*Time.deltaTime);
-        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref cameraVelocity, smoothTime);
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref _cameraVelocity, smoothTime);
     }
 }
